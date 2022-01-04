f8153ffa92d614af70cbf845682bbb3a4c923e3f
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-04 04:51:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Write a Python program to perform the following logical operations. Give Precedence order of logical operators. 
+#Given x=True, y=False
+# x and not y
+# not x and y 
+# not x and not y 
+# x or not y 
+# not x or y 
+# not x or not y  
+x=True
+y=False
+print(x and not y)
+print(not x and y)
+print(not x and not y )
+print(x or not y)
+print(not x or y)
+print(not x or not y)
+  
